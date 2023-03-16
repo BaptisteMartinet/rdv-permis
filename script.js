a81704ed1a6605c1ea6e7d3cc2dadf40f5174506
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-16 17:42:01 +0100

    feat: allow script to check for multiple counties

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const Millisecond = 1;
 const Seconde = Millisecond * 1000;
 const Minute = Seconde * 60;
 
-const FetchingIntervalMs = 10 * Minute;
-const CountyCode = '077';
+const FetchingIntervalMs = 5 * Minute;
+const CountyCodes = ['077', '002', '045', '060'];
 
+let idx = 0;
 setInterval(async () => {
-  const slots = await fetch(`https://candidat.permisdeconduire.gouv.fr/api/v1/candidat/creneaux?code-departement=${CountyCode}`).then(data => data.json()).catch((e => console.error(e)));
+  const countyCode = CountyCodes[idx++ % CountyCodes.length];
+  const slots = await fetch(`https://candidat.permisdeconduire.gouv.fr/api/v1/candidat/creneaux?code-departement=${countyCode}`).then(data => data.json()).catch((e => console.error(e)));
   const now = new Date();
-  console.info(now, slots);
-  localStorage.setItem(now.getTime().toString(), slots?.toString());
+  console.info(`County: ${countyCode}`, now, slots);
 }, FetchingIntervalMs);
 
 console.info('Fetching slots...');
