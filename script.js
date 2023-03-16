4f10561fdabec787d6b74f4761cc6fe3ce33eee7
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-16 17:53:47 +0100

    pref: minor

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const CountyCodes = ['077', '002', '045', '060'];
 
 let idx = 0;
 setInterval(async () => {
-  const countyCode = CountyCodes[idx++ % CountyCodes.length];
+  const countyCode = CountyCodes[idx];
   const slots = await fetch(`https://candidat.permisdeconduire.gouv.fr/api/v1/candidat/creneaux?code-departement=${countyCode}`).then(data => data.json()).catch((e => console.error(e)));
   const now = new Date();
   console.info(`County: ${countyCode}`, now.toUTCString(), slots);
+  idx = idx + 1 % CountyCodes.length;
 }, FetchingIntervalMs);
