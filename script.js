68cf0cd6bf51c4de55222ce528fa8561e2f311d0
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-16 11:13:54 +0100

    refactor: minor

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const Seconde = Millisecond * 1000;
 const Minute = Seconde * 60;
 
 const FetchingIntervalMs = 10 * Minute;
-const countyCode = '077';
+const CountyCode = '077';
 
 setInterval(async () => {
-  const slots = await fetch(`https://candidat.permisdeconduire.gouv.fr/api/v1/candidat/creneaux?code-departement=${countyCode}`).then(data => data.json()).catch((e => console.error(e)));
+  const slots = await fetch(`https://candidat.permisdeconduire.gouv.fr/api/v1/candidat/creneaux?code-departement=${CountyCode}`).then(data => data.json()).catch((e => console.error(e)));
   const now = new Date();
   console.info(now, slots);
   localStorage.setItem(now.getDate().toString(), slots?.toString());
